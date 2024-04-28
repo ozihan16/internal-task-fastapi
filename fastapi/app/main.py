c8584b68a53e774b2a3eaa1ebd0f97f1e052cffd
ozihan16 <87209082+ozihan16@users.noreply.github.com>
2024-04-28 22:07:06 +0300

    Update main.py

--- fastapi/app/main.py
+++ fastapi/app/main.py
@@ -11,10 +11,10 @@ app = FastAPI()
 
 @app.get("/")
 def hello_world():
-    should_fail_500 = percentile_true(10)
+    should_fail_500 = percentile_true(40)
     if should_fail_500:
         raise HTTPException(status_code=500, detail="[500] Internal Server Error")
-    should_fail_400 = percentile_true(10)
+    should_fail_400 = percentile_true(20)
     if should_fail_400:
         raise HTTPException(status_code=400, detail="[400] Bad Request")
     should_fail_404 = percentile_true(10)
